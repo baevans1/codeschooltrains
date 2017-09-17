e9345deeb906310a7d47fa376dc9944ad72d5853
Beth Evans <baevans_87@hotmail.com>
2017-09-17 18:56:55 -0500

    added nested conditionals

--- scripts/trains.js
+++ scripts/trains.js
@@ -1,4 +1,4 @@
-var trainsOperational = 8;
+var trainsOperational = 0;
 var totalTrains = 12;
 //
 // console.log("There are " + trainsOperational + " running trains.");
@@ -26,4 +26,31 @@ for (var trainNumber = 1; trainNumber <= totalTrains; trainNumber++) {
   } else {
     console.log("Train #" + trainNumber + " is not operational.");
   }
+}
+for (var trainNumber = 1; trainNumber <= totalTrains; trainNumber++) {
+  if (trainNumber <= trainsOperational) {
+    console.log("Train #" + trainNumber + " is in service.")
+  } else if (trainNumber == 10) {
+    console.log("Train #10 begins running at noon.");
+  } else {
+    console.log("Train #" + trainNumber + " is not in service.");
+  }
+}
+// nested conditionals
+if(trainsOperational > 0) {
+  if (trainsOperational == totalTrains) {
+    console.log("All trains are running.");
+  } else {
+    for (var trainNumber =1; trainNumber <= totalTrains; trainNumber++) {
+      if (trainNumber <= trainsOperational) {
+        console.log("Train #" + trainNumber + " is up and running.");
+      } else if (trainNumber == 10) {
+        console.log("Train #10 begins running at noon.");
+      } else {
+        console.log("Train #" + trainNumber + " is not in service.");
+      }
+    }
+  }
+} else {
+  console.log("Trains are not in service today.");
 }
