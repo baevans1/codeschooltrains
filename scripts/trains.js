9c22388f02af84a2f2f25e47aefe97a78cfd6c81
Beth Evans <baevans_87@hotmail.com>
2017-09-17 17:29:19 -0500

    added for loop

--- scripts/trains.js
+++ scripts/trains.js
@@ -1,12 +1,22 @@
 var trainsOperational = 8;
 var totalTrains = 12;
-
-console.log("There are " + trainsOperational + " running trains.");
-console.log(trainsOperational == totalTrains);
-console.log("Train #" + 1 + " is running.");
+//
+// console.log("There are " + trainsOperational + " running trains.");
+// console.log(trainsOperational == totalTrains);
+// console.log("Train #" + 1 + " is running.");
 
 var trainNumber = 1;
 while (trainNumber <= trainsOperational) {
   console.log("Train #" + trainNumber + " is running.");
   trainNumber++;
+}
+for (var trainNumber = 1; trainNumber <= trainsOperational; trainNumber++) {
+  console.log("Train #" + trainNumber + " is running.")
+}
+for (var number = 5 ; number > 0; number--) {
+  console.log(number);
+}
+
+for (var stoppedTrain = trainsOperational + 1; stoppedTrain <= totalTrains; stoppedTrain++) {
+  console.log("Train #" + stoppedTrain + " is not running today.");
 }
